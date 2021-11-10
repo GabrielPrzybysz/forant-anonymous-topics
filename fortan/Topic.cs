66926040908a0a3be486659f58c64f2e9a6acef4
Przybysz <gabriel.junior@aquiris.com.br>
2021-11-09 23:10:39 -0300

    Topic Object

---
+++ fortan/Topic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fortan
+{
+    [Serializable]
+    public class Topic
+    {
+
+        public Topic(string id, string author, string title, string text)
+        {
+            Id = id;
+            Author = author;
+            Title = title;
+            Text = text;
+        }
+
+        public string Id { get; set; }
+
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
